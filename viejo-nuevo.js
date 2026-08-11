db86d319bcdeba3e0cb41825fd688bcd11daa65e
Tania Trujillo <tanisfernanda.ans@gmail.com>
2026-08-11 10:26:52 -0500

    se crea una nueva pagina donde se da la opcion para escoger el tipo de carro que se desea escoger

---
+++ viejo-nuevo.js
@@ -0,0 +1,11 @@
+
+
+const menuHamburguesa = document.getElementById('menu-hamburguesa');
+
+if (menuHamburguesa) {
+  menuHamburguesa.addEventListener('click', function() {
+    alert('Menú de navegación desplegado.');
+  });
+}
+
+
